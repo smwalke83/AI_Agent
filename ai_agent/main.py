bdd8aceda4c4ca0d0e7f2655b05231e9d07cbd1f
smwalke83 <smwalk83@gmail.com>
2025-08-08 11:23:44 -0600

    Verbose command working

---
+++ ai_agent/main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    if not args:
+        print('Usage: uv run main.py "your prompt here"')
+        sys.exit(1)
+    prompt = " ".join(args)
+    if verbose:
+        print("User prompt:", prompt)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
